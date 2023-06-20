939a4ac4e03538617fa67d2bd94cdc4c20ac2a5f
Lucas Moreira <lucasdanmoreira@gmail.com>
2023-06-20 15:26:17 +0100

    Pagination

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,6 +1,7 @@
 class TimelineController < ApplicationController
   def index
     @message  = Message.new
-    @messages = Message.order(created_at: :desc)
+
+    @pagy, @messages = pagy(Message.order(created_at: :desc))
   end
 end
